04d45ce7121f37dc3e02d418a4e1859b2dece4cf
eYdr1en <54525514+eYdr1en@users.noreply.github.com>
2023-04-06 13:15:26 +0200

    latest

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -14,40 +14,69 @@ export class AddressByNetwork {
 let network: string = dataSource.network();
 
 let controllerAddressByNetwork: AddressByNetwork = {
-  mainnet: '0xC128468b7Ce63eA702C1f104D55A2566b13D3ABD',
+  mainnet: '0x6a7E068Fe310531dd82c1EC26897fEe6F78dED5f',
   goerli: '0xBB1CE49b16d55A1f2c6e88102f32144C7334B116',
 };
 
-function forNetwork(addressByNetwork: AddressByNetwork, network: string): Address {
+function forNetwork(
+  addressByNetwork: AddressByNetwork,
+  network: string,
+): Address {
   if (network == 'mainnet') {
     return Address.fromString(addressByNetwork.mainnet);
   }
   return Address.fromString(addressByNetwork.goerli);
 }
 
-export const CONTROLLER_ADDRESS = forNetwork(controllerAddressByNetwork, network);
-export const VAULT_ADDRESS = Address.fromString('0xBA12222222228d8Ba445958a75a0704d566BF2C8');
+export const CONTROLLER_ADDRESS = Address.fromString(
+  '0x6a7E068Fe310531dd82c1EC26897fEe6F78dED5f',
+);
+export const VAULT_ADDRESS = Address.fromString(
+  '0x3F39DDE7BF3e17Db8F7D56942B1A2C2880C2e622',
+);
 
-export const ARBITRUM_ROOT_GAUGE_FACTORY = Address.fromString('0xad901309d9e9DbC5Df19c84f729f429F0189a633');
-export const OPTIMISM_ROOT_GAUGE_FACTORY = Address.fromString('0x3083A1C455ff38d39e58Dbac5040f465cF73C5c8');
-export const POLYGON_ROOT_GAUGE_FACTORY = Address.fromString('0x4C4287b07d293E361281bCeEe8715c8CDeB64E34');
+export const ARBITRUM_ROOT_GAUGE_FACTORY = Address.fromString(
+  '0xad901309d9e9DbC5Df19c84f729f429F0189a633',
+);
+export const OPTIMISM_ROOT_GAUGE_FACTORY = Address.fromString(
+  '0x3083A1C455ff38d39e58Dbac5040f465cF73C5c8',
+);
+export const POLYGON_ROOT_GAUGE_FACTORY = Address.fromString(
+  '0x4C4287b07d293E361281bCeEe8715c8CDeB64E34',
+);
 
-export const MAINNET_GAUGE_V2_FACTORY = Address.fromString('0xf1665E19bc105BE4EDD3739F88315cC699cc5b65');
-export const ARBITRUM_ROOT_GAUGE_V2_FACTORY = Address.fromString('0x1c99324EDC771c82A0DCCB780CC7DDA0045E50e7');
-export const GNOSIS_ROOT_GAUGE_V2_FACTORY = Address.fromString('0x2a18B396829bc29F66a1E59fAdd7a0269A6605E8');
-export const OPTIMISM_ROOT_GAUGE_V2_FACTORY = Address.fromString('0x866D4B65694c66fbFD15Dd6fa933D0A6b3940A36');
-export const POLYGON_ROOT_GAUGE_V2_FACTORY = Address.fromString('0xa98Bce70c92aD2ef3288dbcd659bC0d6b62f8F13');
+export const MAINNET_GAUGE_V2_FACTORY = Address.fromString(
+  '0xf1665E19bc105BE4EDD3739F88315cC699cc5b65',
+);
+export const ARBITRUM_ROOT_GAUGE_V2_FACTORY = Address.fromString(
+  '0x1c99324EDC771c82A0DCCB780CC7DDA0045E50e7',
+);
+export const GNOSIS_ROOT_GAUGE_V2_FACTORY = Address.fromString(
+  '0x2a18B396829bc29F66a1E59fAdd7a0269A6605E8',
+);
+export const OPTIMISM_ROOT_GAUGE_V2_FACTORY = Address.fromString(
+  '0x866D4B65694c66fbFD15Dd6fa933D0A6b3940A36',
+);
+export const POLYGON_ROOT_GAUGE_V2_FACTORY = Address.fromString(
+  '0xa98Bce70c92aD2ef3288dbcd659bC0d6b62f8F13',
+);
 
 export function isArbitrumFactory(factory: Address): boolean {
-  return [ARBITRUM_ROOT_GAUGE_FACTORY, ARBITRUM_ROOT_GAUGE_V2_FACTORY].includes(factory);
+  return [ARBITRUM_ROOT_GAUGE_FACTORY, ARBITRUM_ROOT_GAUGE_V2_FACTORY].includes(
+    factory,
+  );
 }
 
 export function isOptimismFactory(factory: Address): boolean {
-  return [OPTIMISM_ROOT_GAUGE_FACTORY, OPTIMISM_ROOT_GAUGE_V2_FACTORY].includes(factory);
+  return [OPTIMISM_ROOT_GAUGE_FACTORY, OPTIMISM_ROOT_GAUGE_V2_FACTORY].includes(
+    factory,
+  );
 }
 
 export function isPolygonFactory(factory: Address): boolean {
-  return [POLYGON_ROOT_GAUGE_FACTORY, POLYGON_ROOT_GAUGE_V2_FACTORY].includes(factory);
+  return [POLYGON_ROOT_GAUGE_FACTORY, POLYGON_ROOT_GAUGE_V2_FACTORY].includes(
+    factory,
+  );
 }
 
 export function isGnosisFactory(factory: Address): boolean {
